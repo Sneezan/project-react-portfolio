af575f6ff637e46111d5eb4490d6c7973bcbb0e4
Cecilia <ceciliafraanlund@gmail.com>
2023-11-02 15:15:22 +0100

    updated employer info

--- src/components/AboutSection.js
+++ src/components/AboutSection.js
@@ -1,48 +1,30 @@
 import React from 'react';
 import { Section, Wrapper } from 'components/Section';
-import { About, AboutWrapper, Line, Dot, LinkedBtn, LinkedPrompt, Website } from 'components/About.js'
-import LInkedIn from '../images/video1.png'
+import { About, AboutWrapper, Line, Dot, Website } from 'components/About.js'
 
 const AboutSection = () => {
   return (
     <Section primary>
       <Wrapper>
         <AboutWrapper>
-          <About> <Line /><Dot /> I´m Cecilia - a social, out-going and ambitious
+          <About> <Line /><Dot /> I´m Cecilia - a social, out-going
             frontend developer with love for code and design. I have extensive experience from
             working towards clients from a previous career in dentistry and I love meeting
             new people. I thrive in challenging environments where I strive to deliver
             out-of-the-box solutions. With an eagerness to learn, I´m constantly looking for
             oppurtunities to deepen my existing knowledge as well as broaden my skill set.
 
-            Currently I´m an intern at
+            Currently I work as a frontend developer at
           <Website>
             <a
-              href="https://stryda.gg/"
+              href="https://www.relexsolutions.com/"
               rel="noreferrer"
-              target="_blank"> Stryda
+              target="_blank"> RELEX Promotions
             </a>
-          </Website>, where I work in an agile team,
-            delivering real functional features and diving into the world of TypeScript and React
-            Query.
-          <br />
-          <br />
-            Click on the play-button for a video pitch!
-          <br />
-          <br />
-          <LinkedPrompt>
-            <br />
-            <a
-              href="https://drive.google.com/file/d/1xzUIfklnnnLkuympUVySEYWT6gcpb453/view?usp=sharing"
-              rel="noreferrer"
-              target="_blank">
-              <LinkedBtn type="button">
-                <img
-                  src={LInkedIn}
-                  alt="link to Cecilias Linked In" />
-              </LinkedBtn>
-            </a>
-          </LinkedPrompt>
+          </Website>, where I work in an team,
+            delivering real functional features and diving into the world of TypeScript and React.
+            If you want to know more about me or what I´m currently working on, please
+            reach out on LinkedIn.
           </About>
         </AboutWrapper>
       </Wrapper>
